f0c4e5f60f35e659853c224e837298b34aca6e49
smilynik <nikola_smilyanski@flutterint.com>
2023-03-12 20:38:41 +0200

    add implementation for Accordion and Blog

--- js-applications/Resource-25856-01.BusStop/05. Accordion/accordion.js
+++ js-applications/Resource-25856-01.BusStop/05. Accordion/accordion.js
@@ -1,3 +1,47 @@
-function solution() {
-    //TODO .....
+ async function solution() {
+    let main = document.getElementById("main");
+
+    let  art =  await fetch('http://localhost:3030/jsonstore/advanced/articles/list');
+    const articles = await art.json();
+
+    for (const a of articles) {
+        let element = document.createElement("div");
+        let getR = await fetch(`http://localhost:3030/jsonstore/advanced/articles/details/${a._id}`)
+        let jsonObkect = await getR.json();
+
+        debugger;
+        element.innerHTML = `
+        <div class="accordion">
+        <div class="head">
+            <span>${a.title}</span>
+            <button class="button" id=${a._id}>More</button>
+        </div>
+        <div class="extra">
+            <p>${jsonObkect.content}</p>
+        </div>
+    </div>`;
+
+    element.querySelector('button').addEventListener('click', (e) =>  {hideAndShowButton(e, element)})
+
+    main.appendChild(element);
+    }
+
+    function hideAndShowButton(e, element) {
+        let button = element.querySelector('button')
+
+        if(button.textContent == 'More') {
+            element.querySelector('.extra').style.display = 'block';
+            button.textContent = 'Less';
+        }else {
+            element.querySelector('.extra').style.display = 'none';
+            button.textContent = 'More';
+        }
+    }
+
+
+
+
 }
+
+
+solution();
--- js-applications/Resource-25856-01.BusStop/06.Blog/app.js
+++ js-applications/Resource-25856-01.BusStop/06.Blog/app.js
@@ -1,5 +1,42 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById("btnLoadPosts").addEventListener('click', loadPosts)
+    document.getElementById("btnViewPost").addEventListener('click', viewComments);
+
+    async function loadPosts() {
+        const posts = document.getElementById("posts");
+       
+
+        const p = await fetch('http://localhost:3030/jsonstore/blog/posts');
+        const postsArray = await p.json();
+
+        for (const p of  Object.values(postsArray)) {
+            const element = document.createElement('option');
+            element.value = p._id;
+            element.textContent = p.title;
+            posts.appendChild(element);
+        }
+    }
+
+    async function viewComments() {
+        const postBody = document.getElementById("post-body");
+        const postComments = document.getElementById("post-comments");
+    
+        const element = document.createElement("li");
+
+        const array = Array(document.querySelectorAll("#posts option")[0]);
+
+       const post = array.find(e => e.checked == true);
+
+       const c = await fetch(`http://localhost:3030/jsonstore/blog/comments/${post.value}`);
+       const comments = await c.json();
+
+       for (const comment of comments) {
+        const li = document.createElement('li');
+        li.textContent = comment.text;
+        postComments.appendChild(li);
+       }
+
+    }
 }
 
 attachEvents();
